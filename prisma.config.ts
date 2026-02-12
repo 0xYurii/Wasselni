f3ed9405e7370eb4916dfb2bb66d550b953c5479
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-02-12 17:37:44 +0100

    feat: configure nodemon for constend reloading

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,9 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
+    schema: "prisma/schema.prisma",
     migrations: {
+        path: "prisma/migrations",
         seed: "tsx prisma/seeds.ts",
     },
     datasource: {
