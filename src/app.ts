6246556f258427ea8519cec5e3fa295c4136f300
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-02-12 17:37:59 +0100

    feat: complete health check

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        credentials: true,
+    }),
+);
+app.use(express.urlencoded({ extended: true }));
+
+//health check route
+app.get("/api/health", (req: Request, res: Response) => {
+    res.status(200).json({ message: "health check is running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
