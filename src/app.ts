6c33ba8c23a14dd838c97baa87df2b0239bdb426
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-03-01 00:06:55 +0100

    feat: move the server side to PORT 5000

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,12 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // CORS
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
         credentials: true,
     }),
 );
