04878e19cf025d85e5e52fc5ea62b630a168bbd4
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-03-01 13:29:02 +0100

    feat: add google signup option

--- src/modules/auth/controller.ts
+++ src/modules/auth/controller.ts
@@ -3,6 +3,59 @@ import prisma from "../../config/prisma.js";
 import { asyncHandler } from "../../core/utils/asyncHandler.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { OAuth2Client } from "google-auth-library";
+
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+export const googleAuth = asyncHandler(async (req: Request, res: Response) => {
+    const { credential } = req.body;
+    if (!credential) {
+        return res.status(400).json({ message: "Google token missing" });
+    }
+
+    const ticket = await googleClient.verifyIdToken({
+        idToken: credential,
+        audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    if (!payload?.email) throw new Error("Invalid Google Payload");
+
+    let user = await prisma.user.findUnique({
+        where: { email: payload.email },
+    });
+
+    if (!user) {
+        await prisma.user.create({
+            data: {
+                email: payload.email,
+                fullName: payload.name || "Google User",
+                googleId: payload.sub,
+                avatar: payload.picture,
+                emailVerified: true,
+            },
+        });
+    }
+
+    const token = jwt.sign(
+        { sub: String(user?.id), role: user?.role },
+        process.env.JWT_SECRET!,
+        {
+            expiresIn: "1d",
+        },
+    );
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+        message: "Google Auth successful",
+        user: { id: user!.id, fullName: user!.fullName, email: user!.email },
+    });
+});
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
     const { email, password } = req.body;
--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, signup, getCurrentUser } from "./controller.js";
+import { login, signup, getCurrentUser,googleAuth } from "./controller.js";
 import { authenticateToken } from "../../core/middleware/auth.middleware.js";
 import { validate } from "../../core/middleware/validate.js";
 import {
@@ -11,6 +11,7 @@ const authRoute = Router();
 
 authRoute.post("/login", validate(loginBodySchema, "body"), login);
 authRoute.post("/signup", validate(signupBodySchema, "body"), signup);
+authRoute.post("/google", googleAuth);
 
 authRoute.get("/me", authenticateToken, getCurrentUser);
 
