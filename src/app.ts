e62511d08cd31955224d3d154dfc5b7391566da2
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-04-07 14:02:20 +0100

    feat: add /api to auth route

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.get("/api/health", (req: Request, res: Response) => {
 });
 
 //Auth route
-app.use("/auth", authRoute);
+app.use("/api/auth", authRoute);
 
 //Rides route
 app.use("/api/rides", ridesRoute);
