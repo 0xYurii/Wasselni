20db883c998e83797ab62f6b8f300ca56caff675
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-04-10 00:33:07 +0100

    feat: Add rate limiting and secure http headers with helmet

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response } from "express";
 import { errorHandler } from "./core/middleware/error.middleware.js";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -6,13 +6,27 @@ import authRoute from "./modules/auth/route.js";
 import ridesRoute from "./modules/rides/route.js";
 import bookingsRoute from "./modules/bookings/route.js";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
-// CORS
+//Rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: { success: false, message: "Too many requests" },
+});
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    message: { success: false, message: "Too many login attempts" },
+});
+
+// CORS & Helmet
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "http://localhost:3000",
@@ -22,6 +36,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api", limiter);
+app.use("/api/auth/login", authLimiter);
+app.use(helmet());
 
 //health check route
 app.get("/api/health", (req: Request, res: Response) => {
